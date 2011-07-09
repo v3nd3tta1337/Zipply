d3d322c8facda48bbc9f6599215a07e5598200de
Xavier Decuyper <xavier.decuyper@gmail.com>
2011-07-09 20:13:58 +0200

    Added license info (GPLv3)

--- core.logic.js
+++ core.logic.js
@@ -1,13 +1,19 @@
 /*
-	This file, core.logic.js, is part of ZIPPLY.
+	core.logic.js
+	This file is part of Zipply.
 
-	(c) Copyright 2011 Xavier Decuyper
-		All Rights Reserved
+	Zipply is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-	THIS IS A DEVELOPMENT SNAPSHOT
-	
-	YOU ARE NOT ALLOWED TO DISTRIBUTE THIS FILE, THIS PROJECT
-	OR THE SOURCE CODE UNTIL FURTHER NOTICE.
+	Zipply is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Zipply.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 var debug = false; //Debugging is off
